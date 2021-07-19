9e3bc05c8fbfe0a08807fec683b80e0b32e26cb5
Henry Leto <hnr.leto11@gmail.com>
2021-07-19 08:59:09 -0400

    add prettier config + build out ws service

--- client/src/services/wsdata-service.service.ts
+++ client/src/services/wsdata-service.service.ts
@@ -1,14 +1,38 @@
 import { Injectable } from '@angular/core';
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
-// import { environment } from '../../environments/environment';
-import { catchError, tap, switchAll } from 'rxjs/operators';
+import { environment } from '../environments/environment';
+import { catchError, tap, switchAll, map } from 'rxjs/operators';
 import { EMPTY, Subject } from 'rxjs';
-export const WS_ENDPOINT = environment.wsEndpoint;
+export const WS_ENDPOINT = environment.endpoint;
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class WSDataServiceService {
+    private socket$!: WebSocketSubject<any>;
 
-  constructor() { }
+    constructor() {}
+
+    public connect() {
+        if (!this.socket$ || this.socket$.closed) {
+            this.socket$ = this.getNewEndpoint();
+            const position = this.socket$.pipe(
+                map(data => {
+                    
+                })
+            )
+        }
+    }
+
+    sendPosition(msg: any) {
+
+    }
+
+    close() {
+        this.socket$.complete();
+    }
+
+    private getNewEndpoint() {
+        return webSocket(WS_ENDPOINT);
+    }
 }
